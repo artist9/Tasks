1a5967904e53338e826c9fa02fefec41779a7a7a
Artist <krlmnhzn@gmail.com>
2023-06-27 17:51:37 +0300

    Сбалансированы ли скобки?

--- my.js
+++ my.js
@@ -1,16 +1,13 @@
-function diff(a, b) {
-  //if (a === 0) a = 360;
-  return Math.min(
-    a > b ? a - b : b - a,
-    a === 0 ? 360 - b : b === 0 ? 360 - a : 360
-  );
+function areBracketsBalanced(str) {
+  let count = 0;
+  for (let i of str) {
+    if (i === "(") count++;
+    if (i === ")") count--;
+    if (count < 0) break;
+  }
+  return count === 0;
 }
 
-console.log(diff(0, 45)); // не 315, а 45, потому что 45 меньше
-console.log(diff(0, 180));
-console.log(diff(0, 190)); // не 190, а 170, потому что 170 меньше
-console.log(diff(120, 280));
-console.log(diff(45, 0)); // не 315, а 45, потому что 45 меньше
-console.log(diff(180, 0));
-console.log(diff(190, 0)); // не 190, а 170, потому что 170 меньше
-console.log(diff(120, 280));
+console.log(areBracketsBalanced("(())")); // true
+console.log(areBracketsBalanced("((())")); // false
+console.log(areBracketsBalanced(")((())")); // false
