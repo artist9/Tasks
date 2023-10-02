e10acd4f5b8c0e827dfc7d4afa4eb0bc273e738f
Artist <krlmnhzn@gmail.com>
2023-10-02 17:45:01 +0300

    Инвертированный регистр

--- my.js
+++ my.js
@@ -1,13 +1,6 @@
-export default function isHappyTicket(n) {
-  let a = n.length / 2;
-  return (
-    n
-      .split("")
-      .filter((c, d) => d < a)
-      .reduce((a, b) => a + +b, 0) ===
-    n
-      .split("")
-      .filter((c, d) => d >= a)
-      .reduce((a, b) => a + +b, 0)
-  );
+export default function invertCase(str) {
+  return str
+    .split("")
+    .map((i) => (i === i.toUpperCase() ? i.toLowerCase() : i.toUpperCase()))
+    .join("");
 }
--- take.js
+++ take.js
@@ -1,7 +1,4 @@
-import isHappyTicket from "./my.js";
+import invertCase from "./my.js";
 
-console.log(isHappyTicket("385916")); // true
-console.log(isHappyTicket("231002")); // false
-console.log(isHappyTicket("1222")); // false
-console.log(isHappyTicket("054702")); // true
-console.log(isHappyTicket("00")); // true
+console.log(invertCase("Hello, World!")); // hELLO, wORLD!
+console.log(invertCase("I loVe JS")); // i LOvE js
