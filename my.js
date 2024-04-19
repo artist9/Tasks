e01461d7db2f9977bbe1c8836bb68ac48f7adcdb
Artist <krlmnhzn@gmail.com>
2024-04-19 17:50:56 +0300

    Сортировка массива, пузырьковая

--- my.js
+++ my.js
@@ -1,9 +1,15 @@
-export default function countUniqChars(str) {
-  let set = new Set(
-    str
-      .split("")
-      .map((i) => i.toLowerCase())
-      .filter((i) => i != " ")
-  );
-  return set.size;
+export default function bubbleSort(arr) {
+  let a = arr.length;
+  if (a === 0) return [];
+  do {
+    for (let i = 0; i < a - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        let b = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = b;
+      }
+    }
+    a--;
+  } while (a);
+  return arr;
 }
--- take.js
+++ take.js
@@ -1,15 +1,5 @@
-import countUniqChars from "./my.js";
+import bubbleSort from "./my.js";
 
-const text1 = "yyab"; // y, a, b
+console.log(bubbleSort([]));
 
-console.log(countUniqChars(text1)); // 3
-
-const text2 = "You know nothing Jon Snow";
-
-console.log(countUniqChars(text2)); // 13
-
-// Если передана пустая строка, то функция должна вернуть `0`
-
-const text3 = "";
-
-console.log(countUniqChars(text3)); // 0
+console.log(bubbleSort([3, 10, 4, 3]));
